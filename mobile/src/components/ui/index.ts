ded6a664314a98e2c2857331dba618226d185a62
adeel86 <38898975+adeel86@users.noreply.github.com>
2026-02-22 11:36:49 +0000

    Removing old MD files and mobile app folder.

--- mobile/src/components/ui/index.ts
+++
@@ -1,5 +0,0 @@
-export { Button } from './Button';
-export { Card, CardHeader, CardContent, CardFooter } from './Card';
-export { Avatar } from './Avatar';
-export { Badge } from './Badge';
-export { Input } from './Input';
